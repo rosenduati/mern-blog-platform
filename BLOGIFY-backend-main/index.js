8b2401e2670e397a4539b92af3e2ee46942909e5
rosenduati <rose.nduati064@gmail.com>
2025-03-25 10:13:43 +0300

    initial commit

---
+++ BLOGIFY-backend-main/index.js
@@ -0,0 +1,34 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/user.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+// Correcting the CORS origin to allow the entire domain, not just a specific path
+app.use(cors({
+  origin: 'https://blogify-mern-4663.vercel.app', // Allowing requests from the front-end origin
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true // This allows cookies to be sent with requests
+}));
+
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) // Ensuring compatibility with new MongoDB driver
+  .then(() => 
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
